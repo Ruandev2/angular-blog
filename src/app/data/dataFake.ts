c098d582579373a2e394ff1a7eed45ed3a875604
Ruan <rupriwell123@outlook.com>
2023-10-23 20:19:03 -0300

    Conclusao Projeto Angula

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://buteconerd.com.br/wp-content/uploads/2023/08/04.08-luffy-gear-five.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
